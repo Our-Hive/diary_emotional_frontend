1037524289a7d1f7096d087bbf5313870563ec0c
SnakyDH <skdanielg5@gmail.com>
2024-03-29 10:24:55 -0500

    feat: optimize provider and add format to files

--- lib/config/router/app_router.dart
+++ lib/config/router/app_router.dart
@@ -11,20 +11,22 @@ import 'package:emotional_app/features/my_space/ui/screens/my_space_view.dart';
 import 'package:emotional_app/config/router/app_paths.dart';
 import 'package:go_router/go_router.dart';
 
-final appRouter = GoRouter(initialLocation: AppPaths.logIn, routes: [
-  GoRoute(
-    path: AppPaths.logIn,
-    builder: (context, state) => const LoginScreen(),
-  ),
-  GoRoute(
-    path: AppPaths.signUp,
-    builder: (context, state) => const SignUpStepAccountScreen(),
-  ),
-  GoRoute(
-    path: AppPaths.signUpContact,
-    builder: (context, state) => const SignUpStepContactScreen(),
-  ),
-  ShellRoute(
+final appRouter = GoRouter(
+  initialLocation: AppPaths.logIn,
+  routes: [
+    GoRoute(
+      path: AppPaths.logIn,
+      builder: (context, state) => const LoginScreen(),
+    ),
+    GoRoute(
+      path: AppPaths.signUp,
+      builder: (context, state) => const SignUpStepAccountScreen(),
+    ),
+    GoRoute(
+      path: AppPaths.signUpContact,
+      builder: (context, state) => const SignUpStepContactScreen(),
+    ),
+    ShellRoute(
       builder: (context, state, child) => HoneLayout(childView: child),
       routes: [
         GoRoute(
@@ -57,5 +59,7 @@ final appRouter = GoRouter(initialLocation: AppPaths.logIn, routes: [
           path: AppPaths.mySpace,
           builder: (context, state) => const MySpaceView(),
         ),
-      ])
-]);
+      ],
+    )
+  ],
+);
